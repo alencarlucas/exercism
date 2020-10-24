a7f595b07518b63c6215c39c588b7e9970245c43
alencarlucas <alencarlucaas@gmail.com>
2020-10-24 14:41:11 -0300

    refactor: Use String methods to evaluate input

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,29 +1,35 @@
 defmodule Bob do
-  @input_types [
-    {~r/((^[[:cntrl:][:blank:]]+$)|(^$))/, :silence},
-    {~r/^[[:upper:]][[:upper:] \d\W]+\?$/u, :yell_question},
-    {~r/(^.*?)((\?$)|(\?[[:blank:]]+))$/u, :question},
-    {~r/^[\d\W]+$/, :unknow},
-    {~r/^[[:upper:] \d\W]+$/u, :yell},
-  ]
+  defp reply([:question, :yell]), do: "Calm down, I know what I'm doing!"
+  defp reply([:question]), do: "Sure."
+  defp reply([:yell]), do: "Whoa, chill out!"
+  defp reply([:silence]), do: "Fine. Be that way!"
+  defp reply([]), do: "Whatever."
 
-  defp reply(:question), do: "Sure."
-  defp reply(:yell), do: "Whoa, chill out!"
-  defp reply(:yell_question), do: "Calm down, I know what I'm doing!"
-  defp reply(:silence), do: "Fine. Be that way!"
-  defp reply(_), do: "Whatever."
-
-  def evaluate_input(input, {regex, _}) do
-    String.match?(input, regex)
+  defp is_yell?(input) do
+    String.upcase(input) == input and String.match?(input, ~r/[[:alpha:]]/)
   end
 
-  def get_input_type(input) do
-    {_, input_type} = Enum.find(@input_types, {nil, nil}, &evaluate_input(input, &1))
-    input_type
+  defp is_question?(input) do
+    String.last(input) == "?"
+  end
+
+  defp is_silence?(input) do
+    input == ""
+  end
+
+  defp get_input_type(input) do
+    cond do
+      input |> is_silence? -> [:silence]
+      input |> is_question? and input |> is_yell? -> [:question, :yell]
+      input |> is_question? -> [:question]
+      input |> is_yell? -> [:yell]
+      true -> []
+    end
   end
 
   def hey(input) do
     input
+    |> String.trim()
     |> get_input_type()
     |> reply()
   end
