3b06950469fb72ad403676022d52156560c7b030
alencarlucas <alencarlucaas@gmail.com>
2020-10-28 00:10:53 -0300

    refactor: Improve module readability

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -1,39 +1,33 @@
 defmodule BeerSong do
-  defp get_verse(0) do
-    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
   end
 
-  defp get_verse(1) do
-    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
   end
 
-  defp get_verse(2) do
-    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-  end
-
-  defp get_verse(number) do
-    "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{
-      number - 1
-    } bottles of beer on the wall.\n"
-  end
-
-  defp concatenate_lyrics(number, "") do
-    verse(number)
-  end
-
-  defp concatenate_lyrics(number, lyrics) do
-    lyrics <> "\n" <> verse(number)
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
   end
 
   def verse(number) do
-    get_verse(number)
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
   end
 
-  def lyrics(range) do
-    Enum.reduce(range, "", &concatenate_lyrics(&1, &2))
-  end
-
-  def lyrics() do
-    lyrics(99..0)
+  def lyrics(range \\ 99..0) do
+    Enum.map_join(range, "\n", &verse(&1))
   end
 end
