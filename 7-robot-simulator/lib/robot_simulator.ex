fef3b5f99944f7904d0916be13fdb6d2830ea85e
alencarlucas <alencarlucaas@gmail.com>
2021-04-02 16:05:59 -0300

    feat: Add solution to Robot Simulator

--- 7-robot-simulator/lib/robot_simulator.ex
+++ 7-robot-simulator/lib/robot_simulator.ex
@@ -1,11 +1,81 @@
 defmodule RobotSimulator do
+  @directions [:north, :east, :south, :west]
+
+  defp setDirection(:north), do: :north
+  defp setDirection(:east), do: :east
+  defp setDirection(:south), do: :south
+  defp setDirection(:west), do: :west
+  defp setDirection(_), do: :error
+
+  defp setPosition({x, y}) when is_integer(x) and is_integer(y), do: {x, y}
+  defp setPosition(_), do: :error
+
+  defp changePosition(direction, {x, y}) do
+    case direction do
+      :north -> {x, y + 1}
+      :east -> {x + 1, y}
+      :south -> {x, y - 1}
+      :west -> {x - 1, y}
+    end
+  end
+
+  defp changeDirection(currentIndex, instructionMove) do
+    @directions
+    |> Enum.at(currentIndex + instructionMove, :north)
+  end
+
+  defp move(direction, position, "R") do
+    newDirection = changeDirection(@directions |> Enum.find_index(fn x -> x == direction end), 1)
+    create(newDirection, position)
+  end
+
+  defp move(direction, position, "L") do
+    changeDirection(@directions |> Enum.find_index(fn x -> x == direction end), -1)
+    |> create(position)
+  end
+
+  defp move(direction, position, "A") do
+    newPosition = changePosition(direction, position)
+    create(direction, newPosition)
+  end
+
+  defp move(_, _, _), do: :error
+
+  @doc """
+  Create a Robot Simulator given an initial direction and position.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec create :: %{direction: :north, position: {0, 0}}
+  def create(), do: %{:direction => :north, :position => {0, 0}}
+
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ nil, position \\ nil) do
+  def create(direction, position) do
+    robot = %{:direction => setDirection(direction), :position => setPosition(position)}
+
+    cond do
+      robot[:direction] == :error ->
+        {:error, "invalid direction"}
+
+      robot[:position] == :error ->
+        {:error, "invalid position"}
+
+      true ->
+        robot
+    end
+  end
+
+  def simulate(:error, _) do
+    {:error, "invalid instruction"}
+  end
+
+  def simulate(robot, "") do
+    robot
   end
 
   @doc """
@@ -15,6 +85,8 @@ defmodule RobotSimulator do
   """
   @spec simulate(robot :: any, instructions :: String.t()) :: any
   def simulate(robot, instructions) do
+    {currentAction, leastInstructions} = instructions |> String.split_at(1)
+    move(direction(robot), position(robot), currentAction) |> simulate(leastInstructions)
   end
 
   @doc """
@@ -24,6 +96,7 @@ defmodule RobotSimulator do
   """
   @spec direction(robot :: any) :: atom
   def direction(robot) do
+    robot[:direction]
   end
 
   @doc """
@@ -31,5 +104,6 @@ defmodule RobotSimulator do
   """
   @spec position(robot :: any) :: {integer, integer}
   def position(robot) do
+    robot[:position]
   end
 end
